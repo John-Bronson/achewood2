dab26172850ee06edaf22f9103a6677326289687
John Bronson <jbronson@gmail.com>
2022-03-16 16:52:17 -0500

    the front end is now rendering ray's blog posts

--- src/BlogPost.jsx
+++ src/BlogPost.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
 const BlogPost = (props) => (
-  <div className="blogPost">
-    <h3 onClick={() => console.log(props)}>{props.currentPost.title}</h3>
-    <img src={props.currentPost.profileURL.profilePicURL} />
+  <div>
+    <h2 onClick={() => console.log(props)}>{props.currentPost.title}</h2>
     <div dangerouslySetInnerHTML={{ __html: props.currentPost.content }} />
-    <p>Posted by {props.currentPost.profileURL.name} at {props.currentPost.published}</p>
-    <a href={props.currentPost.url}>link to original post</a>
   </div>
 )
 
