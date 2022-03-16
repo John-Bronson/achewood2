f40bdc454482f526d54ccd7fb55b8ab3037e4e9f
John Bronson <jbronson@gmail.com>
2022-03-16 15:05:25 -0500

    backend is now able to test dates passed from the front end with dates in the blog posts at the blogger API and tell whether they occured on the same day or not

---
+++ src/ComicStrip.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ComicStrip extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h2 >Comic Strip</h2>
+    )
+  }
+}
+
+export default ComicStrip
